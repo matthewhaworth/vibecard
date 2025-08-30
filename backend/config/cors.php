097467feae29697286835d4f4ea2795e9f820939
Matthew Haworth <matthewh2008@googlemail.com>
2025-08-30 18:04:26 +0100

    Add new urls to cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000','https://vibecard.co.uk', 'https://www.vibecard.co.uk'],
 
     'allowed_origins_patterns' => [],
 
